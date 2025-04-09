62cccda2953950656ace65d5b016b053b33b09cb
noranhe <63988546+noranhe@users.noreply.github.com>
2025-04-09 15:24:37 +0800

    [Mod] 更新说明文档和变更日志

--- vnpy_xt/__init__.py
+++ vnpy_xt/__init__.py
@@ -21,13 +21,11 @@
 # SOFTWARE.
 
 
-import importlib_metadata
-
 from .xt_datafeed import XtDatafeed as Datafeed
 from .xt_gateway import XtGateway
 
 
-try:
-    __version__ = importlib_metadata.version("vnpy_xt")
-except importlib_metadata.PackageNotFoundError:
-    __version__ = "dev"
+__all__ = ["Datafeed", "XtGateway"]
+
+
+__version__ = "1.3.0"
